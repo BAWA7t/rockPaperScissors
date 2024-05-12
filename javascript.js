040e0dc6fcc99a86753ebd70837fbab7fd967216
Bawa <tasonmaxwell7t@gmail.com>
2024-05-12 21:56:32 +0000

    created getComputer() to randomly return item from listItem This will random generate an item by the computer

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("Hello world");
+
+
+ //create a list containing rock paper scissors
+ let listItem = ["rock","paper","scissors"];
+
+//create a function called getComputerChoice
+function getComputerChoice(){
+   //randomly return "rock","paper","scissors" from a list
+   return(listItem[(Math.floor(Math.random() * listItem.length))]);
+
+}
+//getComputerChoice();
+    
+//create a function called getHumanChoice
+    //prompt the user to enter a string
