42fd539b9a1448d280f8f6be20a776dd4b241d9a
Bawa <tasonmaxwell7t@gmail.com>
2024-05-12 22:17:40 +0000

    created getHumanChoice() function This will prompt the user to enter a string as input and convert input to lowerCase

--- javascript.js
+++ javascript.js
@@ -6,10 +6,16 @@
 //create a function called getComputerChoice
 function getComputerChoice(){
    //randomly return "rock","paper","scissors" from a list
+   //getComputerChoice();
    return(listItem[(Math.floor(Math.random() * listItem.length))]);
-
 }
-//getComputerChoice();
+
     
 //create a function called getHumanChoice
+function getHumanChoice(){
     //prompt the user to enter a string
+    //return the input
+    return(prompt("rock,paper or scissors ?").toLowerCase());
+}
+ //getHumanChoice();
+ //
