bd25863126d146516fc22ceaf99ecd71f3e1bf9a
Bawa <tasonmaxwell7t@gmail.com>
2024-05-13 00:28:06 +0000

    playround() is created to play the game for once while checking conditions of the game This is done to break the creation of the game into function
    playgame() is created to play the game for 5 times and output the winner at the end og the game

--- javascript.js
+++ javascript.js
@@ -17,5 +17,55 @@ function getHumanChoice(){
     //return the input
     return(prompt("rock,paper or scissors ?").toLowerCase());
 }
- //getHumanChoice();
- //
+//getHumanChoice();
+//create a variable to store human score
+let HumanScore = 0
+//create a variable to store computer score
+let ComputerScore = 0
+
+//create a function called playRound
+//Define two parameters humanChoice and computerChoice
+function playRound(humanChoice, computerChoice){
+    // make humanChoice case insensitive
+    humanChoice = humanChoice.toLowerCase();
+    //checking a tie
+    if(humanChoice === computerChoice){
+        //console.log("Its a tie")
+    }else if(
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        //console.log("Human wins! "  +  humanChoice + " beats "  + computerChoice);
+        HumanScore++;
+    } else {
+        //console.log("Computer wins! " +  computerChoice + " beats " + humanChoice);
+        ComputerScore++;
+    }
+}
+
+    
+
+
+
+
+
+function playgame(){
+    for (i = 0; i < 5; i++){
+        const  humanSelection = getHumanChoice();
+        const  computerSelection = getComputerChoice();
+        playRound(humanSelection,computerSelection);
+    }
+
+    //Declare the winner at the end of the game
+    if(HumanScore > ComputerScore) {
+        console.log("Human wins!");
+    }else if (HumanScore < ComputerScore){
+        console.log("Computer wins!");
+    }else {
+        console.log("It's a tie game")
+    }
+}
+
+// starts game
+playgame();
